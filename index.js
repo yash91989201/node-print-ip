3fe9dd51b92c3f2ecc42723076a4fd41509d09c1
yash91989201 <yashraj6370@gmail.com>
2023-10-04 21:24:06 +0530

    update port

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+server.listen(80, () => {
+  console.log("Server is running on port 80");
 });
 
 function getPublicIpAddress() {
